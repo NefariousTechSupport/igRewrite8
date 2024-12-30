9e610cdad1806c8e8007bc0ca47f23a1cb616b85
NefariousTechSupport <Jaskaran7852@gmail.com>
2024-12-30 14:33:00 +0000

    Add method comments for Blob

--- Potion/Blob.cs
+++ Potion/Blob.cs
@@ -3,22 +3,41 @@ using System.Text.RegularExpressions;
 
 namespace Potion
 {
+	/// <summary>
+	/// Represents an identifier for binary data
+	/// </summary>
 	public struct Blob
 	{
+		/// <summary>
+		/// A blob that represents nothing
+		/// </summary>
 		public static readonly Blob NullBlob = new Blob { _sha1 = new byte[SHA1.HashSizeInBytes] };
 
 
 		private byte[] _sha1;
 
+
+		/// <summary>
+		/// The sha1 hash for the blob
+		/// </summary>
 		public byte[] Sha1 => _sha1;
 
 
+		/// <summary>
+		/// Private constructor for the blob
+		/// </summary>
+		/// <param name="sha1">The 20 byte long array</param>
 		private Blob(byte[] sha1)
 		{
 			_sha1 = sha1;
 		}
 
 
+		/// <summary>
+		/// Create a new blob
+		/// </summary>
+		/// <param name="sha1">The hex string for the blob, must be 40 characters</param>
+		/// <returns>The blob on success, or null on failure</returns>
 		public static Blob? Construct(string hexString)
 		{
 			if (string.IsNullOrEmpty(hexString)
@@ -38,6 +57,11 @@ namespace Potion
 		}
 
 
+		/// <summary>
+		/// Create a new blob
+		/// </summary>
+		/// <param name="sha1">The hex byte array for the blob, must be 20 bytes</param>
+		/// <returns>The blob on success, or null on failure</returns>
 		public static Blob? Construct(byte[] sha1)
 		{
 			if (sha1 == null
@@ -50,6 +74,12 @@ namespace Potion
 		}
 
 
+		/// <summary>
+		/// Whether the two are equal
+		/// </summary>
+		/// <param name="a">The first blob</param>
+		/// <param name="b">The second blob</param>
+		/// <returns>Whether the two are equal</returns>
 		public static bool operator==(Blob a, Blob b)
 		{
 			for (int i = 0; i < SHA1.HashSizeInBytes; i++)
@@ -64,6 +94,12 @@ namespace Potion
 		}
 
 
+		/// <summary>
+		/// Whether the two aren't equal
+		/// </summary>
+		/// <param name="a">The first blob</param>
+		/// <param name="b">The second blob</param>
+		/// <returns>Whether the two aren't equal</returns>
 		public static bool operator!=(Blob a, Blob b)
 		{
 			for (int i = 0; i < SHA1.HashSizeInBytes; i++)
@@ -78,6 +114,10 @@ namespace Potion
 		}
 
 
+		/// <summary>
+		/// Converts this blob into its string representation
+		/// </summary>
+		/// <returns></returns>
 		public override string ToString()
 		{
 			return Convert.ToHexString(_sha1);
