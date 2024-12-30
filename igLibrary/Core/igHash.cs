ae690886088139daea643e5034de2833813a72ce
NefariousTechSupport <Jaskaran7852@gmail.com>
2024-12-30 14:32:00 +0000

    Style igHash

--- igLibrary/Core/igHash.cs
+++ igLibrary/Core/igHash.cs
@@ -3,12 +3,27 @@ using System.Text;
 
 namespace igLibrary.Core
 {
+	/// <summary>
+	/// Wrapper for Fnv1a hashing
+	/// </summary>
 	public static class igHash
 	{
+		/// <summary>
+		/// Hashes a string to a 32 bit uint with Fnv1a
+		/// </summary>
+		/// <param name="input">The string</param>
+		/// <returns>The hash</returns>
 		public static uint Hash(string input)
 		{
 			return Fnv1a.Hash32(Encoding.ASCII.GetBytes(input));
 		}
+
+
+		/// <summary>
+		/// Hashes a string as lowercase to a 32 bit uint with Fnv1a
+		/// </summary>
+		/// <param name="input">The string</param>
+		/// <returns>The hash</returns>
 		public static uint HashI(string input)
 		{
 			return Fnv1a.Hash32(Encoding.ASCII.GetBytes(input.ToLower()));
