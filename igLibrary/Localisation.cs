3dbff6f6bb06544e7c1c671926b04ff42cf91bb5
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2025-04-27 14:19:07 +0100

    Remove some excess whitespace

--- igLibrary/Localisation.cs
+++ igLibrary/Localisation.cs
@@ -10,6 +10,6 @@ namespace igLibrary
 	{
 		// These exist to fix #102, otherwise people in other countries may get weird bugs
 		public const NumberStyles kENNumberStyles = NumberStyles.AllowDecimalPoint; 
-		public static readonly CultureInfo  kENCultureInfo  = CultureInfo.CreateSpecificCulture("en");
+		public static readonly CultureInfo kENCultureInfo = CultureInfo.CreateSpecificCulture("en");
 	}
 }
