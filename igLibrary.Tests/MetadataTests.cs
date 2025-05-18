8140093d338b9edde9cdd0e8abf815ed631b2945
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2025-05-18 20:41:49 +0100

    Manually correct default for igSamplerStateDesc._hashDirty

--- igLibrary.Tests/MetadataTests.cs
+++ igLibrary.Tests/MetadataTests.cs
@@ -153,4 +153,43 @@ public class MetadataTest
 	/// </summary>
 	[Fact]
 	public void ImaginatorsVfxModulationHelper()   => VfxModulationHelperChecks(igArkCore.EGame.EV_SkylandersImaginators);
+
+
+
+	/// <summary>
+	/// Ensure that the metadata for igSamplerStateBundleDescMetaField has been corrected
+	/// The one from the metadata dump forgets to list the default for a non-persistent boolean
+	/// </summary>
+	private void SamplerStateBundleDescChecks(igArkCore.EGame game)
+	{
+		igArkCore.ReadFromXmlFile(game);
+
+		igCompoundMetaFieldInfo? samplerStateBundleMeta = igArkCore.GetCompoundFieldInfo("igSamplerStateBundleDescMetaField");
+		Assert.NotNull(samplerStateBundleMeta);
+
+		igMetaField? hashDirtyField = samplerStateBundleMeta.GetFieldByName("_hashDirty");
+
+		Assert.NotNull(hashDirtyField);
+
+		Assert.IsType<igBoolMetaField>(hashDirtyField);
+
+		Assert.NotNull(hashDirtyField._default);
+		Assert.True((bool)hashDirtyField._default);
+	}
+
+
+
+	/// <summary>
+	/// Ensure that the metadata for igVfxModulationHelper has been corrected
+	/// </summary>
+	[Fact]
+	public void SuperChargersSamplerStateBundleDesc() => SamplerStateBundleDescChecks(igArkCore.EGame.EV_SkylandersSuperchargers);
+
+
+
+	/// <summary>
+	/// Ensure that the metadata for igVfxModulationHelper has been corrected
+	/// </summary>
+	[Fact]
+	public void ImaginatorsSamplerStateBundleDesc()   => SamplerStateBundleDescChecks(igArkCore.EGame.EV_SkylandersImaginators);
 }
