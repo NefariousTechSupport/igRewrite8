16e69b581edbe3933ce694baccc8f1c323e6cf7f
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2025-08-16 11:56:17 +0100

    Format igName.cs

--- igLibrary/Core/igName.cs
+++ igLibrary/Core/igName.cs
@@ -9,20 +9,52 @@
 
 namespace igLibrary.Core
 {
+	/// <summary>
+	/// Represents a name along with its fnv1a32 hash
+	/// </summary>
 	[igStruct]
 	public struct igName
 	{
 		public string _string = string.Empty;
 		public uint _hash = 0;
-		public igName(){}
+
+
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		public igName()
+		{
+		}
+
+
+
+		/// <summary>
+		/// Constructor taking in a name 
+		/// </summary>
+		/// <param name="name">The name to set</param>
 		public igName(string name)
 		{
 			SetString(name);
 		}
+
+
+
+		/// <summary>
+		/// Constructor taking in a hash without a string
+		/// </summary>
+		/// <param name="hash">The hash to set</param>
 		public igName(uint hash)
 		{
 			_hash = hash;
 		}
+
+
+
+		/// <summary>
+		/// Set the string value and update the hash
+		/// </summary>
+		/// <param name="newString">The new string to set it to</param>
 		public void SetString(string? newString)
 		{
 			if (newString == null || newString == "(null)")
@@ -36,9 +68,16 @@ namespace igLibrary.Core
 				_hash = igHash.HashI(newString);
 			}
 		}
-        public override string ToString()
-        {
-			return _string ?? _hash.ToString("x");
-        }
-    }
+
+
+
+		/// <summary>
+		/// Allow for printing the igName, either showing the string or the fnv1a32 hash in hex
+		/// </summary>
+		/// <returns>The string representation</returns>
+		public override string ToString()
+		{
+			return _string ?? _hash.ToString("08X");
+		}
+	}
 }
