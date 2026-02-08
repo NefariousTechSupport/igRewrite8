ad5469343ca4b4dd323b66b3427223e723302a85
bone <hejsanhejsan030@gmail.com>
2026-02-08 16:29:43 +0100

    Add files via upload

---
+++ igCauldron3/Frames/TextEdit/Input/EditorKeybindAction.cs
@@ -0,0 +1,7 @@
+namespace igCauldron3.Frames.TextEdit.Input;
+
+/// <summary>
+/// Delegate type for actions that are invoked due to a key binding.
+/// </summary>
+/// <returns>True if the key was handled</returns>
+public delegate bool EditorKeybindAction(TextEditor editor, object? context);
