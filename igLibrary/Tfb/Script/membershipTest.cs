735bd537f1785f1cb1a8818b61b664221e5ccbcd
bone <hejsanhejsan030@gmail.com>
2026-02-18 21:31:09 +0100

    Add files via upload

---
+++ igLibrary/Tfb/Script/membershipTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igLibrary.Tfb.Script
+{
+    public enum membershipTest
+    {
+        includes = 0,
+        excludes = 1,
+        intersects_with = 2,
+        includes_all = 3
+    }
+}
