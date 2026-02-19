b294e12cca77a55bc602f62205a568c1ff09a603
bone <hejsanhejsan030@gmail.com>
2026-02-19 15:17:33 +0100

    added sequence/clonesequence

---
+++ igLibrary/Tfb/Script/ClonedSequence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace igLibrary.Tfb.Script
+{
+    public class ClonedSequence : Sequence
+    {
+        public ScriptGroupStack _clones;
+        public ClonedSequence _parent;
+        public static object? _interface; // InterfaceResolver
+    }
+}
