0abbbbe5d16799200717d62166edcfaf079224b8
NefariousTechSupport <64808609+NefariousTechSupport@users.noreply.github.com>
2026-05-25 20:25:27 +0100

    Add [igStruct] attributes to all command parameters

--- igLibrary/Graphics/igCommandParameters.cs
+++ igLibrary/Graphics/igCommandParameters.cs
@@ -14,10 +14,12 @@ using igLibrary.Gfx;
 namespace igLibrary.Graphics
 {
 
+	[igStruct]
 	public struct igCommandSetPrimitiveTypeParameters
 	{
 		public IG_GFX_DRAW _type;
 	}
+	[igStruct]
 	public struct igCommandSetVertexBufferParameters
 	{
 		public igGraphicsObject? _resource;
@@ -25,36 +27,43 @@ namespace igLibrary.Graphics
 		public int _offset;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetIndexBufferParameters
 	{
 		public igGraphicsObject? _resource;
 		public IG_INDEX_TYPE _format;
 		public int _offset;
 	}
+	[igStruct]
 	public struct igCommandSetVertexShaderParameters
 	{
 		public igGraphicsObject? _resource;
 	}
 
+	[igStruct]
 	public struct igCommandSetVertexShaderVariantParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetVertexShaderTextureParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetVertexShaderSamplerParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetViewportParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetScissorParameters
 	{
 		public int _x;
@@ -62,62 +71,76 @@ namespace igLibrary.Graphics
 		public int _w;
 		public int _h;
 	}
+	[igStruct]
 	public struct igCommandSetScissorEnabledParameters
 	{
 		public bool _enabled;
 	}
+	[igStruct]
 	public struct igCommandSetRasterizeStateBundleParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetPixelShaderParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetPixelShaderVariantParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetPixelShaderTextureParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetPixelShaderSamplerParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetAlphaTestStateBundleParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetBlendStateBundleParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetDepthStateBundleParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetStencilStateBundleParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandSetStencilRefParameters
 	{
 		public uint _stencilRef;
 	}
+	[igStruct]
 	public struct igCommandSetRenderTargetsParameters
 	{
 		public ulong[] _colorTargets;
 		public uint _colorCount;
 		public ulong _depthTarget;
 	}
+	[igStruct]
 	public struct igCommandSetRenderTargetMaskParameters
 	{
 		public igGraphicsObject? _resource;
 	}
+	[igStruct]
 	public struct igCommandXenonSetHiStencilParameters
 	{
 		public bool _state;
@@ -125,15 +148,18 @@ namespace igLibrary.Graphics
 		public IG_GFX_HISTENCIL_FUNCTION _func;
 		public uint _refValue;
 	}
+	[igStruct]
 	public struct igCommandXenonSetFlushHiZStencilParameters
 	{
 		public bool _async;
 	}
+	[igStruct]
 	public struct igCommandXenonSetGprCountsParameters
 	{
 		public uint _vertex;
 		public uint _pixel;
 	}
+	[igStruct]
 	public struct igCommandDrawEdgeGeometryParameters
 	{
 		public igGraphicsObject? _edgeGeometry;
@@ -146,110 +172,131 @@ namespace igLibrary.Graphics
 		public uint _cacheId;
 		public bool _cacheResults;
 	}
+	[igStruct]
 	public struct igCommandPS3SetSCullParameters
 	{
 		public IG_GFX_STENCIL_FUNCTION _function;
 		public uint _refValue;
 		public uint _mask;
 	}
+	[igStruct]
 	public struct igCommandSetConstantBoolParameters
 	{
 		public igGraphicsObject? _resource;
 		public bool _value;
 	}
+	[igStruct]
 	public struct igCommandSetConstantIntParameters
 	{
 		public igGraphicsObject? _resource;
 		public int _value;
 	}
+	[igStruct]
 	public struct igCommandSetConstantFloatParameters
 	{
 		public igGraphicsObject? _resource;
 		public float _value;
 	}
+	[igStruct]
 	public struct igCommandSetConstantVec4fParameters
 	{
 		public igGraphicsObject? _resource;
 		public igVec4f _value;
 	}
+	[igStruct]
 	public struct igCommandSetConstantMatrix44fParameters
 	{
 		public igGraphicsObject? _resource;
 		public igMatrix44f _value;
 	}
+	[igStruct]
 	public struct igCommandSetConstantArrayIntParameters
 	{
 		public igGraphicsObject? _resource;
 		public ulong _value;
 		public int _count;
 	}
+	[igStruct]
 	public struct igCommandSetConstantArrayFloatParameters
 	{
 		public igGraphicsObject? _resource;
 		public ulong _value;
 		public int _count;
 	}
+	[igStruct]
 	public struct igCommandSetConstantArrayVec4fParameters
 	{
 		public igGraphicsObject? _resource;
 		public ulong _value;
 		public int _count;
 	}
+	[igStruct]
 	public struct igCommandSetConstantArrayMatrix44fParameters
 	{
 		public igGraphicsObject? _resource;
 		public ulong _value;
 		public int _count;
 	}
+	[igStruct]
 	public struct igCommandApplyConstantBundleParameters
 	{
 		public igGraphicsObject? _bundle;
 	}
+	[igStruct]
 	public struct igCommandApplyConstantValueListParameters
 	{
 		public igGraphicsObject? _list;
 	}
+	[igStruct]
 	public struct igCommandSetPixelShaderTextureEnabledConstantParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetVertexShaderTextureEnabledConstantParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetPixelShaderTextureSizeConstantParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandSetVertexShaderTextureSizeConstantParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandClearRenderTargetParameters
 	{
 		public igGraphicsObject? _resource;
 		public byte _register;
 	}
+	[igStruct]
 	public struct igCommandDrawPrimitivesParameters
 	{
 		public IG_GFX_DRAW _primitive;
 		public int _numPrimitives;
 		public int _offset;
 	}
+	[igStruct]
 	public struct igCommandDecodeMemoryCommandStreamParameters
 	{
 		public igGraphicsObject? _stream;
 	}
+	[igStruct]
 	public struct igCommandCopyTextureParameters
 	{
 		public igGraphicsObject? _source;
 		public igGraphicsObject? _destination;
 		public igCopyTextureParameters _params;
 	}
+	[igStruct]
 	public struct igCommandUpdateTextureParameters
 	{
 		public ulong _texture;
@@ -259,10 +306,12 @@ namespace igLibrary.Graphics
 		public uint _mipLevel;
 		public uint _flags;
 	}
+	[igStruct]
 	public struct igCommandExecuteCallbackParameters
 	{
 		//It's a struct called _callback
 	}
+	[igStruct]
 	public struct igCommandSetCameraMatricesParameters
 	{
 		public byte _cameraIndex;
@@ -270,6 +319,7 @@ namespace igLibrary.Graphics
 		public byte _previousViewMatrix;
 		public byte _projMatrix;
 	}
+	[igStruct]
 	public struct igCommandComputeAndSetInstanceMatricesParameters
 	{
 		public ulong _modelMatrix;
@@ -277,28 +327,34 @@ namespace igLibrary.Graphics
 		public ushort _matrixConstants;
 		public byte _cameraIndex;
 	}
+	[igStruct]
 	public struct igCommandComputeAndSetInstanceConstantsParameters
 	{
 		public byte _effectFlags;
 		public byte _geometryFlags;
 	}
+	[igStruct]
 	public struct igCommandSetCommonRenderStateParameters
 	{
 		public ushort _commonRenderState;
 	}
+	[igStruct]
 	public struct igCommandSetDitherStateParameters
 	{
 		public bool _enabled;
 		public float _ditherOpacity;
 	}
+	[igStruct]
 	public struct igCommandBeginNamedEventParameters
 	{
 		public string _name;
 	}
+	[igStruct]
 	public struct igCommandEndNamedEventParameters
 	{
 		public int _count;
 	}
+	[igStruct]
 	public struct igCommandIssueBufferedGpuTimestampParameters
 	{
 		public igGraphicsObject? _timestamp;
